2c177e12d8af54234da5aeb4771706b8924f7a58
saleh <ghalayinisaleh9@gmail.com>
2025-05-03 12:30:52 +0300

    edited the user model to check for all fields

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -6,11 +6,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +29,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'comapny_id',
+        'company_id',
         'name',
         'email',
         'password',
