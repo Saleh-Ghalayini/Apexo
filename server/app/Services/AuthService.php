b5765744901dbf22688837a9e52a36d4690c6797
saleh <ghalayinisaleh9@gmail.com>
2025-05-03 07:49:59 +0300

    initiated the AuthService file

---
+++ server/app/Services/AuthService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+
+class AuthService {}
