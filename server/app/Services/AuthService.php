d5f52fc90740664ccbed3ea783db5d9dc943e852
saleh <ghalayinisaleh9@gmail.com>
2025-05-03 12:30:29 +0300

    refactored the register method to check comapny existance in the db

--- server/app/Services/AuthService.php
+++ server/app/Services/AuthService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Models\Company;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
@@ -39,6 +40,15 @@ class AuthService
 
     public function register(array $user_data)
     {
+        $company = Company::where('domain', $user_data['company_domain'])->first();
+
+        if (!$company)
+            $company = Company::create([
+                'name' => $user_data['company_name'],
+                'domain' => $user_data['company_domain'],
+            ]);
+
+        $user_data['company_id'] = $company->id;
         $user_data['password'] = Hash::make($user_data['password']);
 
         $user = User::create($user_data);
@@ -47,6 +57,7 @@ class AuthService
             'success' => true,
             'user' => [
                 'id' => $user->id,
+                'company_id' => $user->company_id,
                 'name' => $user->name,
                 'email' => $user->email,
                 'role' => $user->role,
