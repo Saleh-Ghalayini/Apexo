d8fae52436aac5c402cc4cfbb35a6fd216fc6fbb
saleh <ghalayinisaleh9@gmail.com>
2025-05-03 08:32:24 +0300

    added the logic and functionality to the login method in the AuthService

--- server/app/Services/AuthService.php
+++ server/app/Services/AuthService.php
@@ -8,4 +8,32 @@ use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 
-class AuthService {}
+class AuthService
+{
+    public function login(array $credentials)
+    {
+        $auth_credentials = [
+            'email' => $credentials['email'],
+            'password' => $credentials['password'],
+        ];
+
+        if (!$token = JWTAuth::attempt($auth_credentials))
+            return [
+                'success' => false,
+                'message' => 'Invalid Credentials',
+            ];
+
+        $user = Auth::user();
+
+        return [
+            'success' => true,
+            'token' => $token,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'role' => $user->role,
+            ]
+        ];
+    }
+}
