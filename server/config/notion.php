ba267ee200622ca1fa231a5cf66e398ef018454e
saleh <ghalayinisaleh9@gmail.com>
2025-05-06 01:20:59 +0300

    created the config/notion file to access the env variables related to the notion connection

---
+++ server/config/notion.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api_key' => env('NOTION_API_KEY'),
+    'database_id' => env('NOTION_DB_ID'),
+    'api_url' => env('NOTION_API_URL'),
+    'version' => env('NOTION_API_VERSION', '2022-06-28'),
+];
