6d879eebdd52680e3a8c6d67f1863d625b62f33e
saleh <ghalayinisaleh9@gmail.com>
2025-05-07 20:47:19 +0300

    added the components folder and created the header component in it

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+
+    return (
+        <header>
+            <div className='logo'>
+                <img src="./../assets/apexo_logo.svg" />
+                <h1>Apexo</h1>
+            </div>
+            <div className='nav'>
+                <ul>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#features'>Features</a></li>
+                    <li><a href='#pricing'>Pricing</a></li>
+                </ul>
+            </div>
+            <div className='auth'>
+                <button className='login-btn'>Login</button>
+                <button className='start-bth'>Get Started</button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
