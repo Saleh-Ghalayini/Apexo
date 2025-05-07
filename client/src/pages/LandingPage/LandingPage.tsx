b86a3b9f0d59f6403f65c4225c5adadcae16ef51
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 00:56:34 +0300

    implemented the arrow button in the landing page

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+import ArrowDown from '../../components/ArrowDown';
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import './landingPage.css';
@@ -9,6 +10,7 @@ const LandingPage: React.FC = () => {
         <div className='landing-page'>
             <Header />
             <Hero />
+            <ArrowDown />
         </div>
     );
 }
