00ef7e7a579c05ca0bef138ed962e80728965095
saleh <ghalayinisaleh9@gmail.com>
2025-05-07 21:54:45 +0300

    created the landingpage component and loded the header component in it

---
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+
+    return (
+        <Header />
+    );
+}
+
+export default LandingPage
