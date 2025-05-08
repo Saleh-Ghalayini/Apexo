3114e5744a89b0c176d914a5e5fed5767a919ece
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 06:13:30 +0300

    implemeneted the strucutral format of the login page

---
+++ client/src/pages/Auth/Signup/index.tsx
@@ -0,0 +1,74 @@
+import './Login.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
+
+const Login: React.FC = () => {
+
+  return (
+    <div className="login-container">
+      <div className="login-form-container">
+        <div className="login-header">
+          <h1>Welcome Back</h1>
+          <p>Sign in to continue to Apexo</p>
+        </div>
+        
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="example@company.com"
+              required
+            />
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          
+          <div className="form-options">
+            <div className="remember-me">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                name="rememberMe"
+                checked={formData.rememberMe}
+                onChange={handleChange}
+              />
+              <label htmlFor="rememberMe">Remember me</label>
+            </div>
+            <Link to="/forgot-password" className="forgot-password">
+              Forgot password?
+            </Link>
+          </div>
+          
+          <Button type="submit" fullWidth>
+            Sign In
+          </Button>
+        </form>
+        
+        <div className="login-footer">
+          <p>
+            Don't have an account? <Link to="/signup" className="signup-link">Sign Up</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
