a0bb667c69bb1eb97cacea72785590c2d10dc6a4
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 03:39:14 +0300

    implemented a function that will take the user from the About section to the Features section when clicked on the arrow button

--- client/src/components/ArrowDown/index.tsx
+++ client/src/components/ArrowDown/index.tsx
@@ -1,17 +1,33 @@
-import arrow from '../../assets/images/down-arrow.png';
-import Button from '../Button';
 import './ArrowDown.css';
 import React from 'react';
+import arrow from '../../assets/images/down-arrow.png';
 
 const ArrowDown: React.FC = () => {
+    const scrollToNextSection = () => {
+        const about_section = document.getElementById('about');
+        const features_section = document.getElementById('features');
+        
+        if (about_section)
+            if (features_section) {
+                features_section.scrollIntoView({ behavior: 'smooth' });
+            }
+        else
+            window.scrollBy({
+                top: window.innerHeight,
+                behavior: 'smooth'
+            });
+    };
 
     return (
-        <a className='arrow'>
-            <Button className='arrow-btn'>
-                <img src={arrow} />
-            </Button>
-
-        </a>
+        <div className='arrow'>
+            <button 
+                className='arrow-btn'
+                onClick={scrollToNextSection}
+                aria-label="Scroll to next section"
+            >
+                <img src={arrow} alt="Scroll down" />
+            </button>
+        </div>
     );
 }
 
