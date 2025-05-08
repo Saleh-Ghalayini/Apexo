f4a00bdee453662df26c2a2607d9212da77d6d71
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 18:32:53 +0300

    added some routing between the login, signup, and landing pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
 import LandingPage from './pages/LandingPage/LandingPage';
 import './index.css';
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </>
