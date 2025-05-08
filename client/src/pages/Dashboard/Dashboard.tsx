d38861ddc400ba9d894817bfc1065984459ac2db
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 18:49:51 +0300

    initiated the tsx file for the dashboard

---
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      {/* Dashboard content will be added here */}
+    </div>
+  );
+};
+
+export default Dashboard;
