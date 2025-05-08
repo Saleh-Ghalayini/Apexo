78f426b32c4e03f55ee18ef5f3bb79fec89d37a1
saleh <ghalayinisaleh9@gmail.com>
2025-05-08 06:46:55 +0300

    added styling to the login page

---
+++ client/src/pages/Auth/Login/index.tsx
@@ -0,0 +1,104 @@
+import './Login.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/apexo_logo.svg';
+import illustration from '../../../assets/images/illustration.png';
+
+const Login: React.FC = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    rememberMe: false,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // logic will be implemented later, focusing on UI for now
+    console.log('Login form submitted:', formData);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form-container">
+        <div className="login-logo">
+            <img src={logo} alt="Logo" style={{ width: '45px', height: '45px' }} />
+          <span>Apexo</span>
+        </div>
+        <div className="create-account-link">
+          <Link to="/signup">Create an account</Link>
+        </div>
+        <div className="login-form-wrapper">
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <div className="input-wrapper">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Example@gmail.com"
+                required
+              />
+            </div>
+          </div>
+          
+          <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <div className="input-wrapper">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="ApexoP@123"
+                required
+              />
+            </div>
+          </div>
+          
+          <div className="form-options">
+            <div className="remember-me">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                name="rememberMe"
+                checked={formData.rememberMe}
+                onChange={handleChange}
+              />
+              <label htmlFor="rememberMe">Remember me</label>
+            </div>
+            <Link to="/forgot-password" className="forgot-password">
+              Forgot password
+            </Link>
+          </div>
+          
+          <button type="submit" className="sign-in-button" onClick={handleSubmit}>
+            Sign in
+          </button>
+        </div>
+
+        <div className="trouble-signing">
+          <Link to="/help">Trouble signing in?</Link>
+        </div>
+      </div>
+
+      <div className="login-illustration">
+        <img src={illustration} alt="Login illustration" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
