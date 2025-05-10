fb99962c2b84c0d7bab2d7593075b52459f53523
saleh <ghalayinisaleh9@gmail.com>
2025-05-10 03:54:18 +0300

    added the integrations page to the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,19 @@ import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Dashboard from './pages/Dashboard';
+import IntegrationsPage from './pages/Integrations';
 import './index.css';
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
+        <Routes>          
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/integrations" element={<IntegrationsPage />} />
         </Routes>
       </Router>
     </>
