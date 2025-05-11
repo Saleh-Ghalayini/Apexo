b9a1c21b6697d500e08cb386a47ba851e908e317
saleh <ghalayinisaleh9@gmail.com>
2025-05-12 00:08:56 +0300

    created the basic report model

--- server/app/Models/Report.php
+++ server/app/Models/Report.php
@@ -7,22 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    /** @use HasFactory<\Database\Factories\ReportFactory> */
     use HasFactory;
-
-    protected $fillable = [
-        'company_id',
-        'type',
-        'data',
-        'generated_at',
-    ];
-
-    protected $casts = [
-        'data' => 'array'
-    ];
-
-    public function company()
-    {
-        return $this->belongsTo(Company::class);
-    }
 }
