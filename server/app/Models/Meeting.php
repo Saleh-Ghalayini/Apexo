adf2047281649b8c1b2cf321f37bbaeff6117834
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 23:54:12 +0300

    added the fillable fields to the Meeting model

--- server/app/Models/Meeting.php
+++ server/app/Models/Meeting.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Meeting extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'scheduled_at',
+        'ended_at',
+        'transcript',
+        'summary',
+        'status',
+        'external_id',
+        'meeting_url',
+        'attendees',
+        'metadata',
+    ];
 }
