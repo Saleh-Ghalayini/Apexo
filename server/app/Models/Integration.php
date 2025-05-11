ac3bee2a21a9a35696a9ffa6e77b5720becbc882
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 22:01:49 +0300

    created the basic integrations model

--- server/app/Models/Integration.php
+++ server/app/Models/Integration.php
@@ -7,27 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Integration extends Model
 {
+    /** @use HasFactory<\Database\Factories\IntegrationFactory> */
     use HasFactory;
-
-    protected $fillable = [
-        'company_id',
-        'name',
-        'email',
-        'provider',
-        'status',
-        'settings',
-        'connected_at',
-        'disconnected_at',
-    ];
-
-    protected $casts = [
-        'settings' => 'array',
-        'connected_at' => 'datetime',
-        'disconnected_at' => 'datetime'
-    ];
-
-    public function company()
-    {
-        return $this->belongsTo(Company::class);
-    }
 }
