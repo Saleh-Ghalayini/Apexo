9d82d812f23cc95951008f77a3cf67a507883063
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 21:58:04 +0300

    created the basic company model

--- server/app/Models/Company.php
+++ server/app/Models/Company.php
@@ -9,29 +9,4 @@ class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
-
-    protected $fillable = [
-        'name',
-        'domain',
-    ];
-
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
-
-    public function meetings()
-    {
-        return $this->hasMany(Meeting::class);
-    }
-
-    public function reports()
-    {
-        return $this->hasMany(Report::class);
-    }
-
-    public function integrations()
-    {
-        return $this->hasMany(Integration::class);
-    }
 }
