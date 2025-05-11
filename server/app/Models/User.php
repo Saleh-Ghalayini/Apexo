8fdfcc738467a4e1b7d0f52d9548e6f4551bb212
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 21:55:26 +0300

    added user fillable fields with comapny_id and role

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -17,9 +17,16 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'company_id',
         'name',
         'email',
         'password',
+        'role',
+        'job_title',
+        'department',
+        'phone',
+        'avatar',
+        'active',
     ];
 
     /**
@@ -42,56 +49,10 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'active' => 'boolean',
         ];
     }
 
-    <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-    
     /**
      * Get the company that the user belongs to.
      */
@@ -99,5 +60,4 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Company::class);
     }
-}
 }
