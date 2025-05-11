f68f679d3af36975bc3dcfc74b14781f170c0665
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 23:50:48 +0300

    added fillalbe fields to the IntegrationData  model

--- server/app/Models/IntegrationData.php
+++ server/app/Models/IntegrationData.php
@@ -9,4 +9,19 @@ class IntegrationData extends Model
 {
     /** @use HasFactory<\Database\Factories\IntegrationDataFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'integration_id',
+        'data_type',
+        'external_id',
+        'name',
+        'description',
+        'data',
+        'is_active',
+    ];
 }
