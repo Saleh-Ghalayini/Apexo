1215547f0dd24fca5da8105cf25ed58d6c9ffb78
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 22:09:44 +0300

    created the basic ChatSession model

--- server/app/Models/ChatSession.php
+++ server/app/Models/ChatSession.php
@@ -9,14 +9,4 @@ class ChatSession extends Model
 {
     /** @use HasFactory<\Database\Factories\ChatSessionFactory> */
     use HasFactory;
-
-    protected $fillable = [
-        'user_id',
-        'summary',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
