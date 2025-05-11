358c321493120bba0bc67aab6481ff4e8a5ff5fb
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 23:55:19 +0300

    added the tasks relationship to the Meeting model

--- server/app/Models/Meeting.php
+++ server/app/Models/Meeting.php
@@ -47,4 +47,9 @@ class Meeting extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function tasks()
+    {
+        return $this->morphMany(Task::class, 'source');
+    }
 }
