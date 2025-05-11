31f27c9bdfe1cb3b7c518532891036b3fbdf18b3
saleh <ghalayinisaleh9@gmail.com>
2025-05-12 00:09:27 +0300

    added the fillable fields for the report model

--- server/app/Models/Report.php
+++ server/app/Models/Report.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'company_id',
+        'type',
+        'title',
+        'description',
+        'parameters',
+        'data',
+        'period_start',
+        'period_end',
+        'is_scheduled',
+        'schedule',
+        'last_generated_at',
+    ];
 }
