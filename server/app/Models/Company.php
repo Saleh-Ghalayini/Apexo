da3553a9a4aa5a665a42dc30acd275ca899f0a91
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 21:58:37 +0300

    added the fillable fields to the company model

--- server/app/Models/Company.php
+++ server/app/Models/Company.php
@@ -9,4 +9,28 @@ class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'domain',
+        'logo',
+        'description',
+        'address',
+        'city',
+        'state',
+        'country',
+        'zip_code',
+        'phone',
+        'website',
+        'industry',
+        'size',
+        'subscription_ends_at',
+        'subscription_plan',
+        'active',
+    ];
 }
