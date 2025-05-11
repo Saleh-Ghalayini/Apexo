cae23890556f68af9e18b33184a4fb8683fd1f20
saleh <ghalayinisaleh9@gmail.com>
2025-05-11 21:54:00 +0300

    created the base user model

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -2,26 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 
     /**
      * The attributes that are mass assignable.
@@ -29,11 +17,9 @@ class User extends Authenticatable implements JWTSubject
      * @var list<string>
      */
     protected $fillable = [
-        'company_id',
         'name',
         'email',
         'password',
-        'role',
     ];
 
     /**
@@ -57,20 +43,5 @@ class User extends Authenticatable implements JWTSubject
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function company()
-    {
-        return $this->belongsTo(Company::class);
-    }
-
-    public function chatSessions()
-    {
-        return $this->hasMany(ChatSession::class);
-    }
-
-    public function slackAnnouncements()
-    {
-        return $this->hasMany(SlackAnnouncement::class);
     }
 }
