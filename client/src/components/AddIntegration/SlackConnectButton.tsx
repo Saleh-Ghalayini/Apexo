38b9dbbf866190183c3afeae588ad0f73cf4cc20
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 01:54:02 +0300

    feat(SlackConnectButton): added userId to Slack OAuth URL

--- client/src/components/AddIntegration/SlackConnectButton.tsx
+++ client/src/components/AddIntegration/SlackConnectButton.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 const NGROK_BASE = 'https://e5b9-93-126-210-243.ngrok-free.app';
 const SLACK_REDIRECT_PATH = '/api/v1/integrations/slack/redirect';
 
 export default function SlackConnectButton() {
-  const slackUrl = `${NGROK_BASE}${SLACK_REDIRECT_PATH}`;
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    api.get('/user')
+      .then(res => {
+        const id = res.data?.payload?.id || res.data?.id;
+        if (id) setUserId(id.toString());
+      })
+      .catch(() => setUserId(null));
+  }, []);
+
+  const slackUrl = userId
+    ? `${NGROK_BASE}${SLACK_REDIRECT_PATH}?user_id=${userId}`
+    : `${NGROK_BASE}${SLACK_REDIRECT_PATH}`;
 
   return (
     <a href={slackUrl} target="_blank" rel="noopener noreferrer">
-      <button type="button" className="btn btn-slack">
+      <button type="button" className="btn btn-slack" disabled={!userId}>
         Connect Slack
       </button>
     </a>
