d946f0f312e2aa981639b0decbd87ee9b93306c3
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 03:43:07 +0300

    feat(NotionTest): added minimal Notion test component

---
+++ client/src/components/NotionTest/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotionTestComponent: React.FC = () => {
+  return (
+    <div>
+      <h2>Notion Integration Test</h2>
+      <button>Run Tests</button>
+    </div>
+  );
+};
+
+export default NotionTestComponent;
