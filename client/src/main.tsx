c0e5623e0ebfff64965f3bd3dbe0f0b075883514
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 23:17:05 +0300

    feat(main): created and wrapped the main component with auth provider.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
-    <App />,
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )
