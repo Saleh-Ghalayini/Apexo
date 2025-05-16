107de2d16ff14d55e60cef38eeba1581b0b098cc
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 04:40:16 +0300

    feat(AuthContextDefinition): added AuthContextType interface

--- client/src/context/AuthContextDefinition.ts
+++ client/src/context/AuthContextDefinition.ts
@@ -1,3 +1,13 @@
 import { createContext } from 'react';
 
-export const AuthContext = createContext(undefined);
+export interface AuthContextType {
+  user: any;
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<unknown>;
+  register: (userData: any) => Promise<unknown>;
+  logout: () => Promise<void>;
+  loading: boolean;
+  refreshToken: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
