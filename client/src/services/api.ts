4e4b09ff829db183472b5614813ed41993cfc82a
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 05:00:45 +0300

    feat(api): add request interceptor to attach auth token

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -11,4 +11,15 @@ const api: AxiosInstance = axios.create({
   },
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
