0ce1171c76998b7626a8fb10a192062e9c020aa1
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 22:55:41 +0300

    chore(app): started with empty App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/Signup';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Dashboard from './pages/Dashboard';
-import IntegrationsPage from './pages/Integrations';
-import './index.css';
-
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>          
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/integrations" element={<IntegrationsPage />} />
-        </Routes>
-      </Router>
-    </>
-  )
+  return null;
 }
 
-export default App
+export default App;
