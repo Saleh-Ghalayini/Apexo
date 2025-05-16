8f5ab90f96f3c007a31aaba673293c274af3d64a
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 22:56:33 +0300

    chore(app): added BrowserRouter and a single landing page route.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+
 function App() {
-  return <div>Hello, Apexo!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
