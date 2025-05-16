d6790ef489f0c78993eb0f483506c69614ee16f3
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 14:51:27 +0300

    feat(cleanAIResponse): added more characters to be removed when found.

--- client/src/utils/cleanAIResponse.ts
+++ client/src/utils/cleanAIResponse.ts
@@ -4,4 +4,7 @@ export function cleanAIResponse(text: string): string {
     .replace(/#+\s?/g, '') // Remove Markdown headings (###, ##, #)
     .replace(/\*\*/g, '')  // Remove bold
     .replace(/^- /gm, '')   // Remove list dashes at line start
+    .replace(/`/g, '')      // Remove backticks
+    .replace(/---+/g, '')   // Remove horizontal rules
+    .replace(/\n{2,}/g, '\n') // Collapse multiple newlines
 }
