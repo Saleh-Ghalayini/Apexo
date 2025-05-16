e0cff8b71b5b58e01241a0669083208c15a5dfa7
saleh <ghalayinisaleh9@gmail.com>
2025-05-16 22:57:04 +0300

    chore(app): added Dashboard and Integrations routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
+import Dashboard from './pages/Dashboard';
+import IntegrationsPage from './pages/Integrations';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/integrations" element={<IntegrationsPage />} />
       </Routes>
     </Router>
   );
