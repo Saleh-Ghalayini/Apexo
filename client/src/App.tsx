33046327ed56b4856e04b60168a5419c95696ab4
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 02:33:57 +0300

    feat(App): cleaned the App.tsx from the routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,67 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/Signup';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Dashboard from './pages/Dashboard';
-import IntegrationsPage from './pages/Integrations';
-import SlackSuccess from './pages/Integrations/SlackSuccess';
-import NotionSuccess from './pages/Integrations/NotionSuccess';
-import NotionTestComponent from './components/NotionTest';
-import PrivateRoute from './components/PrivateRoute';
-import AuthRedirect from './components/AuthRedirect';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 import { AuthProvider } from './context/AuthContext';
+import { ToastProvider } from './components/Toast/ToastContext';
 import './index.css';
 
 function App() {  
   return (
     <AuthProvider>
-      <Router>
-        <Routes>          
-          <Route path="/" element={<LandingPage />} />
-          <Route 
-            path="/login" 
-            element={
-              <AuthRedirect>
-                <Login />
-              </AuthRedirect>
-            } 
-          />
-          <Route 
-            path="/signup" 
-            element={
-              <AuthRedirect>
-                <Signup />
-              </AuthRedirect>
-            } 
-          />
-          <Route 
-            path="/dashboard" 
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            } 
-          />
-          <Route 
-            path="/integrations" 
-            element={
-              <PrivateRoute>
-                <IntegrationsPage />
-              </PrivateRoute>
-            } 
-          />          
-          <Route path="/integrations/slack/success" element={<SlackSuccess />} />
-          <Route path="/integrations/notion/success" element={<NotionSuccess />} />
-          <Route 
-            path="/notion/test" 
-            element={
-              <PrivateRoute>
-                <NotionTestComponent />
-              </PrivateRoute>
-            } 
-          />
-        </Routes>
-      </Router>
+      <ToastProvider>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </ToastProvider>
     </AuthProvider>
   )
 }
