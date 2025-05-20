8796302c174f6d8ff795b485f95c991d17829b2b
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:42:49 +0300

    seeder(TestMultiCompanySeeder.php): added second test company creation.

--- server/database/seeders/TestMultiCompanySeeder.php
+++ server/database/seeders/TestMultiCompanySeeder.php
@@ -2,12 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class TestMultiCompanySeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $company2 = Company::factory()->create([
+            'name' => 'Second Test Company',
+            'domain' => 'secondtest.com',
+        ]);
     }
 }
