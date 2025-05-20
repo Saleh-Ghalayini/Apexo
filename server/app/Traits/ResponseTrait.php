a4a2f681a61282012be81354d140706d2d8f24aa
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 10:58:28 +0300

    trait(ResponseTrait): added successResponse and errorResponse methods.

--- server/app/Traits/ResponseTrait.php
+++ server/app/Traits/ResponseTrait.php
@@ -4,5 +4,19 @@ namespace App\Traits;
 
 trait ResponseTrait
 {
-    // empty
+    public function successResponse($data, $code = 200)
+    {
+        return response()->json([
+            "success" => true,
+            "payload" => $data
+        ], $code);
+    }
+
+    public function errorResponse($error, $code)
+    {
+        return response()->json([
+            "success" => false,
+            "error" => "An unexpected error occurred. Please try again later."
+        ], $code);
+    }
 }
