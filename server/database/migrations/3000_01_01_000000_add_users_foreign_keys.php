15674532f78fc08f214b0fafa3e07fcb43194bf4
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 07:19:39 +0300

    feat(migrations): added new columns to the db.

--- server/database/migrations/3000_01_01_000000_add_users_foreign_keys.php
+++ server/database/migrations/3000_01_01_000000_add_users_foreign_keys.php
@@ -6,7 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up(): void {}
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('set null');
+        });
+    }
 
-    public function down(): void {}
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+        });
+    }
 };
