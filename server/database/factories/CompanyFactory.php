9335020e7419ef399049cc19ed14512d27cdd48e
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 09:24:54 +0300

    factories(CompanyFactory): created empty CompanyFactory.

--- server/database/factories/CompanyFactory.php
+++ server/database/factories/CompanyFactory.php
@@ -4,21 +4,12 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
- */
 class CompanyFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->company(),
-            'domain' => fake()->unique()->domainName(),
+            // empty
         ];
     }
 }
