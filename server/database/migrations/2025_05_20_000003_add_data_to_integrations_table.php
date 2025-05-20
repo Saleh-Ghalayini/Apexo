d1859b012e7c4f7e965d3db8da064e6a93598bdf
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 07:18:58 +0300

    feat(migrations): .

--- server/database/migrations/2025_05_20_000003_add_data_to_integrations_table.php
+++ server/database/migrations/2025_05_20_000003_add_data_to_integrations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class AddDataToIntegrationsTable extends Migration
 {
     public function up()
     {
@@ -23,4 +23,4 @@ return new class extends Migration
             }
         });
     }
-};
+}
