910263871177b2bcd9d047542be3e34d999dda08
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 09:22:41 +0300

    factories(UserFactory): created empty UserFactory.

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -2,34 +2,14 @@
 
 namespace Database\Factories;
 
-use App\Models\Company;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'password' => static::$password ??= Hash::make('password123'),
-            'role' => fake()->randomElement(['employee', 'manager', 'hr']),
-            'company_id' => Company::factory(),
+            // empty
         ];
     }
 }
