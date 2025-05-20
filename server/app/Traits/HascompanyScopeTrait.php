ec50e1b434712c5767efda4b802a4a04285e0ff4
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 10:59:59 +0300

    trait(HasCompnayScopeTrait): created empty HasCompanyScopeTrait.

--- server/app/Traits/HascompanyScopeTrait.php
+++ server/app/Traits/HascompanyScopeTrait.php
@@ -2,27 +2,7 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Database\Eloquent\Builder;
-
-trait HascompanyScopeTrait
+trait HasCompanyScopeTrait
 {
-    /**
-     * “Booting a trait” in Laravel refers to the process of 
-     * initializing trait behavior in an Eloquent model. 
-     * When a model boots, Laravel automatically calls methods named 
-     * boot{TraitName} for each trait used by the model.
-     */
-    protected static function bootHasCompanyScopeTrait()
-    {
-        static::addGlobalScope(
-            'company',
-            function (Builder $builder) {
-                $user = Auth::user();
-
-                if ($user && $builder->getModel()->getTable() !== 'users')
-                    $builder->where($builder->getModel()->getTable() . '.company_id', $user->company_id);
-            }
-        );
-    }
+    // empty
 }
