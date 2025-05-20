584fbdcb30de1d7ada6790b8e447557c1756ef34
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 21:37:09 +0300

    services(AIToolsService): Added tool service properties and initialized them in the constructor.

--- server/app/Services/AIToolsService.php
+++ server/app/Services/AIToolsService.php
@@ -2,12 +2,25 @@
 
 namespace App\Services;
 
+use App\Services\Tools\ReportToolsService;
+use App\Services\Tools\MeetingToolsService;
+use App\Services\Tools\CalendarToolsService;
+use App\Services\Tools\EmployeeToolsService;
+
 class AIToolsService
 {
     protected $dataAccessService;
+    protected $meetingToolsService;
+    protected $reportToolsService;
+    protected $calendarToolsService;
+    protected $employeeToolsService;
 
     public function __construct($dataAccessService)
     {
         $this->dataAccessService = $dataAccessService;
+        $this->meetingToolsService = new MeetingToolsService($dataAccessService);
+        $this->reportToolsService = new ReportToolsService($dataAccessService);
+        $this->calendarToolsService = new CalendarToolsService($dataAccessService);
+        $this->employeeToolsService = new EmployeeToolsService($dataAccessService);
     }
 }
