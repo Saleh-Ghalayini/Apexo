8c2f9ae68f994049e62d918e7adf77bdf7a03a49
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:49:39 +0300

    model(report): added the fillabes the repot model.

--- server/app/Models/Report.php
+++ server/app/Models/Report.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Report extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'company_id',
+        'type',
+        'title',
+        'description',
+        'parameters',
+        'data',
+        'period_start',
+        'period_end',
+        'is_scheduled',
+        'schedule',
+        'last_generated_at',
+    ];
 }
