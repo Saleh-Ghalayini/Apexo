b858a585fd7840d9c2313b59d1be54255fca5552
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:40:28 +0300

    model(user): added the fillables to the user model.

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -10,4 +10,17 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'company_id',
+        'name',
+        'email',
+        'password',
+        'role',
+        'job_title',
+        'department',
+        'phone',
+        'avatar',
+        'active',
+    ];
 }
