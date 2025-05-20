4bf66a09a8bc89337571a38e5a97062f3d138153
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:55:39 +0300

    model(integrationData): added fillables the integrationData model.

--- server/app/Models/IntegrationData.php
+++ server/app/Models/IntegrationData.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class IntegrationData extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'integration_id',
+        'data_type',
+        'external_id',
+        'name',
+        'description',
+        'data',
+        'is_active',
+    ];
 }
