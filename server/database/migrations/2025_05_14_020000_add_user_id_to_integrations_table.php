4b7e11a4d3c722f7fe4e0db942e5844b09899df0
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 06:14:40 +0300

    feat(migrations): created the add_user_id_to_integrations table migration file.

---
+++ server/database/migrations/2025_05_14_020000_add_user_id_to_integrations_table.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToIntegrationsTable extends Migration
+{
+    public function up() {}
+
+    public function down() {}
+}
