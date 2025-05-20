d07987d87e8895468f44e860d314a286b051bcc4
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 22:39:54 +0300

    services(ToolDispatcherService): Implemented task report and email report request handling in dispatchTool.

--- server/app/Services/ToolDispatcherService.php
+++ server/app/Services/ToolDispatcherService.php
@@ -20,5 +20,23 @@ class ToolDispatcherService
         ];
     }
 
-    public function dispatchTool($session, $user, $userMessage, $userChatMessage) {}
+    public function dispatchTool($session, $user, $userMessage, $userChatMessage)
+    {
+        if ($this->tools['chat_report']->isTaskReportRequest($userMessage)) {
+            Log::info('[ToolDispatcher] Task report request detected');
+            return [
+                'result' => $this->tools['chat_report']->handleTaskReportRequest($session, $userChatMessage),
+                'tool' => 'task_report',
+                'handled' => true,
+            ];
+        }
+        if ($this->tools['chat_report']->isEmailReportRequest($userMessage)) {
+            Log::info('[ToolDispatcher] Email report request detected');
+            return [
+                'result' => $this->tools['chat_report']->handleEmailReportRequest($session, $userChatMessage, $userMessage),
+                'tool' => 'email_report',
+                'handled' => true,
+            ];
+        }
+    }
 }
