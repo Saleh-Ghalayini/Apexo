f663c20b3648e1b55020178b935f0d31d3960f04
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:00:34 +0300

    trait(HasCompnayScopeTrait): completed HasCompanyScopeTrait with global company scope logic.

--- server/app/Traits/HascompanyScopeTrait.php
+++ server/app/Traits/HascompanyScopeTrait.php
@@ -2,10 +2,21 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+
 trait HasCompanyScopeTrait
 {
     protected static function bootHasCompanyScopeTrait()
     {
-        // method skeleton
+        static::addGlobalScope(
+            'company',
+            function (Builder $builder) {
+                $user = Auth::user();
+
+                if ($user && $builder->getModel()->getTable() !== 'users')
+                    $builder->where($builder->getModel()->getTable() . '.company_id', $user->company_id);
+            }
+        );
     }
 }
