9d5417298ec7607f2d1e2626b9a375d5b4c4dff4
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 06:10:13 +0300

    feat(migrations): added the personal_access_tokens schema to the migration file.

--- server/database/migrations/2025_05_12_012638_create_personal_access_tokens_table.php
+++ server/database/migrations/2025_05_12_012638_create_personal_access_tokens_table.php
@@ -6,7 +6,22 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up(): void {}
+    public function up(): void
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
 
-    public function down(): void {}
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
 };
