1d9986db7dd9c132a2d79e2dee1f978c651067e1
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 06:49:44 +0300

    feat(migrations): added new columns to the tasks table.

--- server/database/migrations/2025_05_17_000008_make_source_type_and_source_id_nullable_in_tasks_table.php
+++ server/database/migrations/2025_05_17_000008_make_source_type_and_source_id_nullable_in_tasks_table.php
@@ -6,7 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up(): void {}
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('source_type')->nullable()->change();
+            $table->unsignedBigInteger('source_id')->nullable()->change();
+        });
+    }
 
-    public function down(): void {}
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('source_type')->nullable(false)->change();
+            $table->unsignedBigInteger('source_id')->nullable(false)->change();
+        });
+    }
 };
