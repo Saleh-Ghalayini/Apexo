02c32482c0d04adbeef8cb03dbd60a4fc55cfaa1
saleh <ghalayinisaleh9@gmail.com>
2025-05-21 00:05:17 +0300

    services(AuthService): Added required use statements for dependencies.

--- server/app/Services/AuthService.php
+++ server/app/Services/AuthService.php
@@ -2,6 +2,12 @@
 
 namespace App\Services;
 
+use Exception;
+use App\Models\User;
+use App\Models\Company;
+use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
 class AuthService
 {
     // Empty class for initial atomic commit
