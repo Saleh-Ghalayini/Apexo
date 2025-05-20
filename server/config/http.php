69f5c9eaeeae4cc5ef87757baa9567aabfaf705c
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:17:59 +0300

    config(http): created the http config.

--- server/config/http.php
+++ server/config/http.php
@@ -1,23 +1,3 @@
 <?php
 
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | HTTP Client Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the HTTP client options used by the application,
-    | especially for external API calls like OpenAI.
-    |
-    */
-
-    'timeout' => env('HTTP_TIMEOUT', 30), // Default timeout of 30 seconds
-
-    'connect_timeout' => env('HTTP_CONNECT_TIMEOUT', 15), // Default connect timeout of 15 seconds
-
-    'retry' => [
-        'max_attempts' => env('HTTP_RETRY_MAX_ATTEMPTS', 3),
-        'initial_delay' => env('HTTP_RETRY_INITIAL_DELAY', 1000), // milliseconds
-        'max_delay' => env('HTTP_RETRY_MAX_DELAY', 5000), // milliseconds
-    ],
-];
+return [];
