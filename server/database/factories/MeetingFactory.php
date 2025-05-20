950878c12a3c70c9706d4d8d0d63000641cb7993
saleh <ghalayinisaleh9@gmail.com>
2025-05-21 01:12:50 +0300

    Facotires(MeetingFactory): enhanced the meeting factory.

--- server/database/factories/MeetingFactory.php
+++ server/database/factories/MeetingFactory.php
@@ -9,7 +9,18 @@ class MeetingFactory extends Factory
     public function definition(): array
     {
         return [
-            // empty
+            'user_id' => \App\Models\User::factory(),
+            'title' => $this->faker->sentence(3),
+            'scheduled_at' => $this->faker->dateTimeBetween('-1 week', '+1 week'),
+            'ended_at' => $this->faker->dateTimeBetween('+1 week', '+2 weeks'),
+            'transcript' => $this->faker->paragraph(),
+            'summary' => $this->faker->paragraph(),
+            'status' => 'scheduled',
+            'external_id' => null,
+            'meeting_url' => $this->faker->url(),
+            'attendees' => [],
+            'metadata' => [],
+            'analytics' => null,
         ];
     }
 }
