512c9f2bb0b0f7be2e22e6053d29cd237965f8c2
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:53:46 +0300

    model(meeting): added the fillables the meeting model.

--- server/app/Models/Meeting.php
+++ server/app/Models/Meeting.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Meeting extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'scheduled_at',
+        'ended_at',
+        'transcript',
+        'summary',
+        'status',
+        'external_id',
+        'meeting_url',
+        'attendees',
+        'metadata',
+        'analytics',
+    ];
 }
