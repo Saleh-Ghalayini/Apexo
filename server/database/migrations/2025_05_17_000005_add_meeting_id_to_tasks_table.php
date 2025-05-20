2101276f30d04b8f0f8db38d741de42891434052
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 06:43:06 +0300

    feat(migrations): added the add_meeting_id cloumn to tasks table migration file.

--- server/database/migrations/2025_05_17_000005_add_meeting_id_to_tasks_table.php
+++ server/database/migrations/2025_05_17_000005_add_meeting_id_to_tasks_table.php
@@ -6,7 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up(): void {}
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('meeting_id')->nullable()->after('user_id');
+        });
+    }
 
-    public function down(): void {}
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('meeting_id');
+        });
+    }
 };
