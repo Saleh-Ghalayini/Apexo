757c0eb6fb6ef3b04ba8348b261a21daf00a0343
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:29:25 +0300

    seeder(ChatAITestDataSeeder.php): created empty ChatAITestDataSeeder.

---
+++ server/database/seeders/ChatAITestDataSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ChatAITestDataSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //
+    }
+}
