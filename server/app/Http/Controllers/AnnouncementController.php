990abca19ac092c94c2079925c32a2eebbabd734
saleh <ghalayinisaleh9@gmail.com>
2025-05-21 00:53:22 +0300

    controllers(AnnouncementController): Initialize controller with AnnouncementService dependency and constructor.

--- server/app/Http/Controllers/AnnouncementController.php
+++ server/app/Http/Controllers/AnnouncementController.php
@@ -2,26 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\ResponseTrait;
 use App\Services\AnnouncementService;
-use App\Http\Requests\SendAnnouncementRequest;
 
 class AnnouncementController extends Controller
 {
-    use ResponseTrait;
     protected AnnouncementService $announcementService;
 
     public function __construct(AnnouncementService $announcementService)
     {
         $this->announcementService = $announcementService;
-    }
-
-    public function sendToSlack(SendAnnouncementRequest $request)
-    {
-        $user = $request->user();
-        $data = $request->validated();
-        $result = $this->announcementService->sendToSlack($user, $data);
-        if (!empty($result['success'])) return $this->successResponse($result['announcement']);
-        else return $this->errorResponse($result['error'] ?? 'Failed', $result['code'] ?? 400);
     }
 }
