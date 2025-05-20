6e149f0026811ec82f217ce1d41772ca7a615c87
saleh <ghalayinisaleh9@gmail.com>
2025-05-21 00:54:26 +0300

    controllers(AnnouncementController): Add ResponseTrait and sendToSlack method signature.

--- server/app/Http/Controllers/AnnouncementController.php
+++ server/app/Http/Controllers/AnnouncementController.php
@@ -6,10 +6,14 @@ use App\Services\AnnouncementService;
 
 class AnnouncementController extends Controller
 {
+    use ResponseTrait;
+
     protected AnnouncementService $announcementService;
 
     public function __construct(AnnouncementService $announcementService)
     {
         $this->announcementService = $announcementService;
     }
+
+    public function sendToSlack($request) {}
 }
