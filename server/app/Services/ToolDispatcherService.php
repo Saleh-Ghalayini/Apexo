c1619856ba8db05d2ca2434f73d632e1dbc3a9c9
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 22:39:08 +0300

    services(ToolDispatcherService):  Added tools property and constructor with dependencies.

--- server/app/Services/ToolDispatcherService.php
+++ server/app/Services/ToolDispatcherService.php
@@ -2,4 +2,21 @@
 
 namespace App\Services;
 
-class ToolDispatcherService {}
+class ToolDispatcherService
+{
+    protected array $tools;
+
+    public function __construct(
+        ChatReportService $chatReportService,
+        ChatAiService $chatAiService,
+        AIToolsService $aiToolsService,
+        AIService $aiService
+    ) {
+        $this->tools = [
+            'chat_report' => $chatReportService,
+            'chat_ai' => $chatAiService,
+            'ai_tools' => $aiToolsService,
+            'ai_service' => $aiService,
+        ];
+    }
+}
