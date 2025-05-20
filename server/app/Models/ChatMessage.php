dfd3ca1ff8852576defdf832ef4e7ae0c118e7b5
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 12:10:55 +0300

    model(ChatMessage): added the fillables to the ChatMessage model.

--- server/app/Models/ChatMessage.php
+++ server/app/Models/ChatMessage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'chat_session_id',
+        'role',
+        'content',
+        'metadata',
+        'status',
+    ];
 }
