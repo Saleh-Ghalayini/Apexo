b50fee023ebeb76b02acdaf23b84a5f5634dc010
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:43:45 +0300

    model(user): added the tasks relation to the user model.

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -71,4 +71,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->belongsTo(Company::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
