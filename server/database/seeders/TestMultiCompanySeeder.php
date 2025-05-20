be1db2eb60e0266ea150c8f43bf25a6cf77148c0
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:43:11 +0300

    seeder(TestMultiCompanySeeder.php): added HR user for second company.

--- server/database/seeders/TestMultiCompanySeeder.php
+++ server/database/seeders/TestMultiCompanySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Company;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class TestMultiCompanySeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class TestMultiCompanySeeder extends Seeder
         $company2 = Company::factory()->create([
             'name' => 'Second Test Company',
             'domain' => 'secondtest.com',
+        ]);
+
+        $hr2 = User::factory()->create([
+            'name' => 'HR User Company 2',
+            'email' => 'hr@secondtest.com',
+            'password' => Hash::make('password'),
+            'role' => 'hr',
+            'company_id' => $company2->id,
         ]);
     }
 }
