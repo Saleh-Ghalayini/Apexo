b5fcfc77d8c243cc0d5996532c8685618d1c98d8
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:38:33 +0300

    seeder(TaskSeeder.php): created empty TaskSeeder.

---
+++ server/database/seeders/TaskSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    public function run(): void {}
+}
