886b2244a9c7568b59a10d46c2f6c70aa57389b8
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 12:14:45 +0300

    model(AIPrompt): added the fillables to the  AIPrompt model.

--- server/app/Models/AiPrompt.php
+++ server/app/Models/AiPrompt.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class AiPrompt extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'prompt_text',
+        'description',
+        'category',
+        'parameters',
+        'is_favorite',
+        'is_public',
+        'usage_count',
+    ];
 }
