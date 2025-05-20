76ed3c6e0f8ac25092f8f800ef7f17c2c0beb219
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:29:47 +0300

    seeder(ChatAITestDataSeeder.php): added test company creation.

--- server/database/seeders/ChatAITestDataSeeder.php
+++ server/database/seeders/ChatAITestDataSeeder.php
@@ -2,12 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class ChatAITestDataSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $company = Company::updateOrCreate(
+            ['name' => 'Apexo Test Company'],
+            [
+                'domain' => 'apexo-test.example.com',
+                'address' => '123 Test Street',
+                'phone' => '123-456-7890',
+                'website' => 'https://apexo-test.example.com',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
     }
 }
