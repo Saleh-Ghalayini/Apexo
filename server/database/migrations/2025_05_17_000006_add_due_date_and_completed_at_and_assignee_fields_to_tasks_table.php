ccf5bd9dc1de7ea8510e4f2e83909f6c4df56f5a
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 06:47:04 +0300

    feat(migrations): added the a new columns to the tasks table.

--- server/database/migrations/2025_05_17_000006_add_due_date_and_completed_at_and_assignee_fields_to_tasks_table.php
+++ server/database/migrations/2025_05_17_000006_add_due_date_and_completed_at_and_assignee_fields_to_tasks_table.php
@@ -6,7 +6,20 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up(): void {}
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('due_date')->nullable()->after('priority');
+            $table->timestamp('completed_at')->nullable()->after('due_date');
+            $table->string('assignee_name')->nullable()->after('completed_at');
+            $table->string('assignee_email')->nullable()->after('assignee_name');
+        });
+    }
 
-    public function down(): void {}
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn(['due_date', 'completed_at', 'assignee_name', 'assignee_email']);
+        });
+    }
 };
