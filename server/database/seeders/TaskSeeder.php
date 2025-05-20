dad3dfa47c33a2952e591e1ac48c912218673955
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:39:07 +0300

    seeder(TaskSeeder.php): added hardcoded emails to TaskSeeder.

--- server/database/seeders/TaskSeeder.php
+++ server/database/seeders/TaskSeeder.php
@@ -9,5 +9,13 @@ use Illuminate\Database\Seeder;
 
 class TaskSeeder extends Seeder
 {
-    public function run(): void {}
+    public function run(): void
+    {
+        $emails = [
+            'ghalayinisaleh4@gmail.com',
+            'ghalayinisaleh69@gmail.com',
+            'rghalayini21@gmail.com',
+        ];
+        $users = User::whereIn('email', $emails)->get();
+    }
 }
