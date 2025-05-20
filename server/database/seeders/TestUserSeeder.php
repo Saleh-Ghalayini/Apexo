455af778d1a07bc65d3fa5b44dc897164fda6303
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:48:13 +0300

    seeder(TestUserSeeder.php): added test company creation.

--- server/database/seeders/TestUserSeeder.php
+++ server/database/seeders/TestUserSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class TestUserSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $company = Company::firstOrCreate([
+            'domain' => 'apexotest.com',
+        ], [
+            'name' => 'Apexo Test Company',
+        ]);
     }
 }
