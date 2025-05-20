f4f5daea374ae12086f81518a094b1c3b811912d
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:36:39 +0300

    seeder(DatabaseSeeder.php): added company creation.

--- server/database/seeders/DefaultUserSeeder.php
+++ server/database/seeders/DefaultUserSeeder.php
@@ -8,5 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class DefaultUserSeeder extends Seeder
 {
-    public function run(): void {}
+    public function run(): void
+    {
+        $company = \App\Models\Company::factory()->create([
+            'name' => 'Apexo Demo Company',
+            'domain' => 'apexo.local',
+            'active' => true,
+        ]);
+    }
 }
