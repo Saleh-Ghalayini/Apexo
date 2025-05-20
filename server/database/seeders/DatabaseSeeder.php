b59b28cd7d8480b3fe7942980a0bc9072561db63
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 08:32:53 +0300

    seeder(DatabaseSeeder.php): added demo company creation.

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -8,6 +8,13 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $company = \App\Models\Company::firstOrCreate(
+            [
+                'domain' => 'apexodemo.com',
+            ],
+            [
+                'name' => 'Apexo Demo Company',
+            ]
+        );
     }
 }
