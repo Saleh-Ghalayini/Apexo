3a4aad46fa923d50abed8044e23cdd21178a93b3
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 11:55:21 +0300

    model(integrationData): created the integrationData model.

--- server/app/Models/IntegrationData.php
+++ server/app/Models/IntegrationData.php
@@ -2,44 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class IntegrationData extends Model
 {
-    /** @use HasFactory<\Database\Factories\IntegrationDataFactory> */
     use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'integration_id',
-        'data_type',
-        'external_id',
-        'name',
-        'description',
-        'data',
-        'is_active',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'data' => 'json',
-            'is_active' => 'boolean',
-        ];
-    }
-
-    public function integration()
-    {
-        return $this->belongsTo(Integration::class);
-    }
 }
