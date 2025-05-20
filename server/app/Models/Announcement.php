8f52428af427ebf01ceed6d611fc6d6c9bd3316c
saleh <ghalayinisaleh9@gmail.com>
2025-05-20 12:12:04 +0300

    model(Announcement): initiated the Announcement model.

--- server/app/Models/Announcement.php
+++ server/app/Models/Announcement.php
@@ -8,16 +8,4 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Announcement extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'title',
-        'message',
-        'slack_channel',
-        'user_id',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
